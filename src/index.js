44bdd8020b78086e84031d9d315a7d52f749bf4d
Vendetta <34066247+vendettadelsangue@users.noreply.github.com>
2020-07-17 15:58:17 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ function createWindow() {
   win.on('closed', () => {
     app.quit();
   });
-  win.webContents.openDevTools();
+  // win.webContents.openDevTools(); for Developers
 };
 Menu.setApplicationMenu(null);
 
