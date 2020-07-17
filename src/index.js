338dc70c91eda19405bae558d81a30c3c07e027f
Vendetta <34066247+vendettadelsangue@users.noreply.github.com>
2020-07-17 15:49:58 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-const { app, BrowserWindow, Menu } = require('electron')
-const path = require('path')
-const url = require('url')
-let win
+const { app, BrowserWindow, Menu } = require('electron');
+const url = require('url');
+let win;
 function createWindow() {
-  const paths = path.join
   win = new BrowserWindow({
     width: 1200,
     height: 800,
@@ -12,35 +10,20 @@ function createWindow() {
       nodeIntegrationInWorker: true
     },
     resizable: false
-  })
+  });
   win.loadURL(url.format({
     pathname: paths(__dirname, './view/index.html'),
     protocol: 'file:',
     slashes: false
-  }))
+  }));
 
   win.on('closed', () => {
-
-    app.quit()
-    win = null
-  })
-  win.webContents.openDevTools()
-}
+    app.quit();
+  });
+  win.webContents.openDevTools();
+};
 Menu.setApplicationMenu(null);
 
 app.on('ready', () => {
-  console.log('Program is started')
-  createWindow()
-})
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (win === null) {
-    createWindow()
-  }
-})
+  createWindow();
+});
